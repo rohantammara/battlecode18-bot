5ec841ca4737a986e04f709dc2ea866b548c9532
CodeGod101 <32391406+RamnathKumar98@users.noreply.github.com>
2018-01-23 17:18:36 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -15,9 +15,14 @@ directions = [bc.Direction.North, bc.Direction.Northeast, bc.Direction.East,
 tryRotate = [0,-1,-7,-2,-6,-3,-5]
 mining =  True
 corpus = []
-prev_dir =  {}
+blocked =  {}
+miners=0
+worker_number=0
+global blueprint_number
+blueprint_number=0
 
 print("TestStarter")
+factory_number=0
 
 random.seed(1047)
 
@@ -29,6 +34,14 @@ gc.queue_research(bc.UnitType.Knight)
 
 my_team = gc.team()
 print(my_team)
+
+def lay_blueprint(worker_id):
+    for d in directions:
+        if  gc.can_blueprint(worker_id , bc.UnitType.Factory ,d):
+            gc.blueprint(worker_id, bc.UnitType.Factory ,d)
+            blueprint_number+=1
+        else:
+            continue
 
 def Karbonite_Mining(id,directions,unit,mining):
     karbonite_collected = False
@@ -62,11 +75,55 @@ def fuzzygoto(unit,dest):
     for tilt in  tryRotate:
         d = rotate(toward,tilt)
         if gc.can_move(unit.id,d):
-            gc.move_robot(unit.id,d)
-            break
+            if unit.id not in blocked.keys():
+                 if d == bc.Direction.North:
+                     blocked[unit.id] = [bc.Direction.South,bc.Direction.Southeast,bc.Direction.Southwest]
+                 elif d == bc.Direction.Northeast:
+                     blocked[unit.id] = [bc.Direction.Southwest,bc.Direction.South,bc.Direction.West]
+                 elif d == bc.Direction.East:
+                     blocked[unit.id] = [bc.Direction.West,bc.Direction.Southwest,bc.Direction.Northwest]
+                 elif d == bc.Direction.Southeast:
+                     blocked[unit.id] = [bc.Direction.Northwest,bc.Direction.North,bc.Direction.West]
+                 elif d == bc.Direction.South:
+                     blocked[unit.id] = [bc.Direction.North,bc.Direction.Northeast,bc.Direction.Northwest]
+                 elif d == bc.Direction.Southwest:
+                     blocked[unit.id] = [bc.Direction.Northeast,bc.Direction.North,bc.Direction.East]
+                 elif d == bc.Direction.West:
+                     blocked[unit.id] = [bc.Direction.East,bc.Direction.Northeast,bc.Direction.Southeast]
+                 elif d == bc.Direction.Northwest:
+                     blocked[unit.id] = [bc.Direction.Southeast,bc.Direction.South,bc.Direction.East]
+                 else:
+                     continue
+            for values in blocked[unit.id]:
+                if values == d:
+                    stinky = True
+                    break
+                else:
+                    stinky = False
+            if stinky == False:
+                gc.move_robot(unit.id,d)
+
+                break
+
+    if d == bc.Direction.North:
+        blocked[unit.id] = [bc.Direction.South,bc.Direction.Southeast,bc.Direction.Southwest]
+    elif d == bc.Direction.Northeast:
+        blocked[unit.id] = [bc.Direction.Southwest,bc.Direction.South,bc.Direction.West]
+    elif d == bc.Direction.East:
+        blocked[unit.id] = [bc.Direction.West,bc.Direction.Southwest,bc.Direction.Northwest]
+    elif d == bc.Direction.Southeast:
+        blocked[unit.id] = [bc.Direction.Northwest,bc.Direction.North,bc.Direction.West]
+    elif d == bc.Direction.South:
+        blocked[unit.id] = [bc.Direction.North,bc.Direction.Northeast,bc.Direction.Northwest]
+    elif d == bc.Direction.Southwest:
+        blocked[unit.id] = [bc.Direction.Northeast,bc.Direction.North,bc.Direction.East]
+    elif d == bc.Direction.West:
+        blocked[unit.id] = [bc.Direction.East,bc.Direction.Northeast,bc.Direction.Southeast]
+    elif d == bc.Direction.Northwest:
+        blocked[unit.id] = [bc.Direction.Southeast,bc.Direction.South,bc.Direction.East]
 
 earthMap =  gc.starting_map(bc.Planet.Earth)
-dest = bc.MapLocation(bc.Planet.Earth,(earthMap.width),(earthMap.height))
+centre= bc.MapLocation(bc.Planet.Earth,(earthMap.width)//2,(earthMap.height)//2)
 
 while True:
     print('pyround:', gc.round())
@@ -75,23 +132,86 @@ while True:
 
         for unit in gc.my_units():
             location = unit.location
+            replications=0
+
+            if gc.round()==1:
+                start_Node=location.map_location()
 ## Mining
             if unit.unit_type == bc.UnitType.Worker :
-                if not unit.id in corpus:
+
+                if replications<3 and gc.karbonite()>70 and worker_number<10 and (gc.round())%15==0:
+                    for d in directions:
+                        if gc.can_replicate(unit.id,d):
+                            gc.replicate(unit.id,d)
+                            replications+=1
+                            worker_number+=1
+                        else:
+                            continue
+
+                if not unit.id in corpus and miners<4:
                     corpus.append(unit.id)
+                    miners+=1
+
                 if(mining==True):
                     mining = Karbonite_Mining(unit.id,directions,unit,mining)
 
                     if mining == False:
                         corpus.remove(unit.id)
+                        miners-=1
                         if len(corpus) == 0:
                             mining = False
                         else:
                             mining = True
 ## This if condition only works if worker has done nothing this round.. Put suitable code here!!
-            if mining == False:
-                prev_dir.clear()
-                fuzzygoto(unit,dest)
+            if not unit.id in corpus:
+                    nearby = gc.sense_nearby_units(location.map_location(), 2)
+                    for other in nearby:
+                            if blueprint_number>2 and gc.can_build(unit.id,other.id):
+                                gc.build(unit.id, other.id)
+                                if bc.Unit.structure_is_built(other):
+                                    print("built a factory")
+                                    blueprint_number-=1
+                            elif   gc.can_repair(unit.id,other.id):
+                                 gc.repair(unit.id,other.id)
+                                 print("repaired a factory")
+                                 continue
+                            elif gc.karbonite()>200 and factory_number<5 and gc.round()%20==0:
+                                lay_blueprint(unit.id)
+
+                            else:
+                                direction_to_start_node=unit.location.map_location().direction_to(centre)
+                                ind_for_this=directions.index(direction_to_start_node)
+                                i=0
+                                for tilt in  tryRotate:
+                                    d = rotate(directions[ind_for_this - 4],tilt)
+                                    if gc.can_move(unit.id,d) and gc.is_move_ready(unit.id):
+                                        gc.move_robot(unit.id,d)
+                                        break
+
+            if unit.unit_type == bc.UnitType.Factory :
+                garrison = unit.structure_garrison()
+                if len(garrison)>0:
+                    for d in directions:
+                        if gc.can_unload(unit.id,d):
+                            print("unloaded something")
+                            gc.unload(unit.id,d)
+                            continue
+
+                elif gc.can_produce_robot(unit.id, bc.UnitType.Knight):
+                    gc.produce_robot(unit.id, bc.UnitType.Knight)
+                    print('produced a knight!')
+                    continue
+
+            if  unit.unit_type == bc.UnitType.Knight :
+                close_by=gc.sense_nearby_units(unit.map_location(),30)
+                for enemy in close_by:
+                    if enemy.team !=my_team and gc.is_attack_ready(unit.id) and gc.can_attack(unit.id,enemy.id):
+                        print("attacking a thing")
+                        gc.attack(unit.id,other.id)
+                        continue
+                    else:
+                        if (location.map_location().direction_to(centre) != bc.Direction.Center):
+                            fuzzygoto(unit,centre)
 
     except Exception as e:
         print('Error:', e)
